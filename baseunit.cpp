6cc3419538b628672fbba40b746484aba31c0ee2
Tema <tema@tema-VirtualBox.(none)>
2013-11-14 20:05:14 +0400

    baseunit

--- baseunit.cpp
+++ baseunit.cpp
@@ -5,4 +5,6 @@ BaseUnit::BaseUnit()
     this->image = new QImage(":/graphics/base.png");
     this->frameSize = QSize(32, 32);
     this->frames = 1;
+    this->healthPoints = 100;
+    this->level = 1;
 }
