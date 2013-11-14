524b5cda3b9534320aa3b8ba9235d72cafbef090
F1NAL <f1nal@ubuntu.(none)>
2013-11-14 20:01:06 +0400

    + house image + brand new background

--- baseunit.cpp
+++ baseunit.cpp
@@ -2,7 +2,20 @@
 
 BaseUnit::BaseUnit()
 {
-    this->image = new QImage(":/graphics/base.png");
-    this->frameSize = QSize(32, 32);
+    Initialize();
+}
+
+BaseUnit::BaseUnit(float x, float y)
+{
+    Initialize();
+
+    this->x = x;
+    this->y = y;
+}
+
+void BaseUnit::Initialize()
+{
+    this->image = new QImage(":/graphics/house.png");
+    this->frameSize = QSize(94, 83);
     this->frames = 1;
 }
