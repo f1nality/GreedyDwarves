11b7264e713a2d2be9beca1c6c67de3e7a0aa067
F1NAL <f1nal@ubuntu.(none)>
2013-11-22 21:18:35 +0400

    + fixed archer animation

--- skeletonarcher.cpp
+++ skeletonarcher.cpp
@@ -9,13 +9,14 @@ SkeletonArcher::SkeletonArcher(float x, float y)
 {
     this->x = x;
     this->y = y;
+
     Initialize();
 }
 
 void SkeletonArcher::Initialize()
 {
-    this->image = new QImage(":/graphics/archer.png");
-    this->frameSize = QSize(54, 64);
+    this->image = new QImage(":/graphics/skeletonarcher.png");
+    this->frameSize = QSize(57, 46);
     this->speed = 1;
     this->maxHealthPoints = 20;
     this->healthPoints = maxHealthPoints;
@@ -25,7 +26,11 @@ void SkeletonArcher::Initialize()
     this->frames.insert(0, 3);
     this->frames.insert(1, 3);
 
-    this->attackingFrames.insert(2,5);
-    this->attackingFrames.insert(3,5);
-    this->attackingFrames.insert(4,5);
+    this->attackingFrames.insert(2, 3);
+    this->attackingFrames.insert(3, 9);
+    this->attackingFrames.insert(4, 3);
+    this->attackingFrames.insert(5, 3);
+    this->attackingFrames.insert(6, 3);
+    this->attackingFrames.insert(7, 3);
+    this->attackingFrames.insert(8, 3);
 }
